ccf9a163654ccb2fc69c381aa4838971c1ead7eb
“radhikasct” <radhikasct@gmail.com>
2019-07-31 09:45:00 +0000

    added multiplication method

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -2,9 +2,14 @@ class BasicCalculator {
     public static void main(String args[]) {
         System.out.println("Calculator Program");
         int c = addition(3, 6);
+        int m = multiplication(2,6);
     }
 
     public static int addition(int a, int b) {
         return a + b;
     }
+    public static int multiplication(int a, int b){
+       return a * b;
+   }
+
 }
