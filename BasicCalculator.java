50ef41321c18a6f91dee0dce114ccd46c94242d0
“radhikasct” <radhikasct@gmail.com>
2019-07-31 09:42:55 +0000

    Basic calculator program added

---
+++ BasicCalculator.java
@@ -0,0 +1,10 @@
+class BasicCalculator {
+    public static void main(String args[]) {
+        System.out.println("Calculator Program");
+        int c = addition(3, 6);
+    }
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+}
