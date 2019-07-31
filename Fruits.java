18ad7ef681a9b5e8d76351b2041481bf5392fd25
“radhikasct” <radhikasct@gmail.com>
2019-07-31 09:51:11 +0000

    fruits java program

---
+++ Fruits.java
@@ -0,0 +1,8 @@
+class Fruits{
+   public static void main(String args[]){
+       List<String> fruits = new ArrayList<String>();
+       fruits.add("mango");
+       fruits.add("apple");
+       System.out.println(fruits);
+   }
+}
