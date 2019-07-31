9c5088ad49c2d28ef3fbab8033c94874d60b3112
“radhikasct” <radhikasct@gmail.com>
2019-07-31 09:46:43 +0000

    subtraction method added

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -2,9 +2,14 @@ class BasicCalculator {
     public static void main(String args[]) {
         System.out.println("Calculator Program");
         int c = addition(3, 6);
+        int s = subtraction(6,3);
     }
 
     public static int addition(int a, int b) {
         return a + b;
     }
+    public static int subtraction(int a, int b){
+       return a - b;
+   }
+
 }
